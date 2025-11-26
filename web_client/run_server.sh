5516d7164d733bffc3e16459a803ac903d16e6d0
Gustavo Nunes <gnunes@device-43.home>
2025-11-26 05:15:30 -0300

    improve web run script

--- web_client/run_server.sh
+++ web_client/run_server.sh
@@ -1,1 +1,19 @@
+
+PORT=3000
+
+# Find PID running on the port
+PID=0
+PIDS=$(lsof -t -i :"$PORT")
+# Kill each PID safely
+for PID in $PIDS; do
+  echo "Process with PID $PID running on port $PORT"
+  echo "Killing PID $PID"
+  kill -9 "$PID"
+done
+
+
+if [ -z "$PID" ] || [ "$PID" -eq 0 ]; then
+  echo "No process found running on port $PORT. Lanching server..."
+fi
+
 npm run dev
